12af4d73ceaa6d47ad6591776eda7ee0ea1fd39b
yeonju419 <yeonju419@naver.com>
2016-07-18 17:21:42 +0900

    C:\\ 감지

---
+++ RegFsNotify/RegFsNotify.cpp
@@ -0,0 +1,134 @@
+#include "mon.h"
+
+HANDLE g_hStopEvent;
+HANDLE g_hFile;		// 파일 다루는 핸들
+HANDLE g_hRegWatch[2];
+
+USHORT GetConsoleTextAttribute(HANDLE hConsole)
+{
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	GetConsoleScreenBufferInfo(hConsole, &csbi);
+	return(csbi.wAttributes);
+}
+
+// 콘솔로 출력 함수
+void Output_Console(USHORT Color, LPTSTR format, ... )
+{
+	va_list args;
+	int len;
+	DWORD cb;
+	LPTSTR buffer;
+
+	va_start(args, format);
+	len = _vsctprintf(format, args) + sizeof(TCHAR); 
+	buffer = new TCHAR[len * sizeof(TCHAR)];
+
+	if (!buffer) { 
+		return;
+	}
+
+	_vstprintf_s(buffer, len, format, args);
+
+	HANDLE Handle = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	if (Color) 
+	{ 
+		SetConsoleTextAttribute(
+			Handle, Color | FOREGROUND_INTENSITY);
+	} 
+
+	_tprintf(buffer);
+
+	SetConsoleTextAttribute(Handle, 
+		FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_RED);
+
+	delete[] buffer;
+}
+
+// 텍스트 파일로 출력 함수
+void Output_File(LPTSTR format, ...)
+{
+	va_list args;
+	int len;
+	DWORD cb;
+	LPTSTR buffer;
+
+	va_start(args, format);
+	len = _vsctprintf(format, args) + sizeof(TCHAR); 
+	buffer = new TCHAR[len * sizeof(TCHAR)];
+
+	if (!buffer){	return; }
+
+	_vstprintf_s(buffer, len, format, args);
+
+	if (g_hFile != INVALID_HANDLE_VALUE) {
+#ifdef _UNICODE
+		LPSTR str = new CHAR[len + 1];
+		if (str) { 
+			memset(str, 0, len + 1);
+			WideCharToMultiByte(CP_ACP, 0, buffer, -1, str, len, NULL, NULL);
+			WriteFile(g_hFile, str, strlen(str), &cb, NULL);
+			delete[] str;
+		}
+#else 
+		WriteFile(g_hFile_Prefetch, buffer, strlen(buffer), &cb, NULL);
+#endif
+	}
+
+	delete[] buffer;
+}
+
+//프로그램 종료 시 사용될 부분이라 신경안써도 됨.
+BOOL CtrlHandler(DWORD fdwCtrlType) 
+{ 
+	switch(fdwCtrlType) 
+	{ 
+	case CTRL_C_EVENT: 
+		SetEvent(g_hStopEvent);
+		return TRUE;
+	case CTRL_CLOSE_EVENT: 
+		SetEvent(g_hStopEvent);
+		return TRUE; 
+	case CTRL_BREAK_EVENT: 
+		return FALSE; 
+	case CTRL_LOGOFF_EVENT: 
+		return FALSE; 
+	case CTRL_SHUTDOWN_EVENT: 
+		return FALSE; 
+	default: 
+		return FALSE; 
+	} 
+}
+
+void _tmain(int argc, TCHAR *argv[])
+{
+	checkState();	// [2] 테스트 함수
+
+	// Privilege Setting
+	if( !SetPrivilege(SE_DEBUG_NAME, TRUE) )
+        return;
+
+	makeDummy();
+
+	_tprintf(_T("\n"));
+
+	g_hStopEvent = CreateEvent(NULL, FALSE, FALSE, NULL);
+	SetConsoleCtrlHandler((PHANDLER_ROUTINE)CtrlHandler, TRUE);
+
+	// 파일 생성 : 현재 우리는 파일 출력을 할 필요가 없기 때문에 완전한 결과값 하나만 출력하도록 함
+	g_hFile = CreateFile(_T("KillProcessList.txt"), GENERIC_WRITE, FILE_SHARE_READ, 0, CREATE_ALWAYS, 0, NULL);
+
+	// 쓰레드 생성 및 활동
+	HANDLE hThread[2];
+
+	hThread[0] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)StartFileMonitor, NULL, 0, NULL);
+	hThread[1] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)StartRegistryMonitor, NULL, 0, NULL);
+	
+	WaitForMultipleObjects(2, (const HANDLE*)&hThread, TRUE, INFINITE); 
+
+	// 열려 있는 핸들 종료
+	CloseHandle(g_hStopEvent);
+	CloseHandle(g_hFile);
+
+	_tprintf(_T("Program terminating.\n"));
+}
---
+++ RegFsNotify/mon.h
@@ -0,0 +1,128 @@
+#pragma once
+
+// 헤더파일 선언
+#include <windows.h>
+#include <tchar.h>
+#include <stdio.h>
+#include <string.h>
+#include <locale.h>
+#include <stdlib.h>
+#include <tlhelp32.h>
+#include <time.h>
+
+// [2] 테스트를 위한 추가
+#include <locale.h>	
+#include <shlobj.h>					// for IsUserAnAdmin() function
+#pragma comment(lib, "shell32.lib")	// for IsUserAnAdmin() function
+void checkState();			// 점검 함수 추가
+
+// [3] 테스트를 위한 추가
+#include "tlhelp32.h"
+#define DEF_PROC_NAME	(L"RegFsNotify.exe")	//(L"svchost.exe")
+#define DEF_DLL_NAME	(L"myhack.dll")
+int setUser();				// 사용자 권한 함수 추가
+
+#define STATUS_SUCCESS ((NTSTATUS)0x00000000L)
+#define MAX_VALUE_NAME 16383
+#define FILE_CHANGE_FLAGS FILE_NOTIFY_CHANGE_FILE_NAME |\
+	FILE_NOTIFY_CHANGE_DIR_NAME |\
+	FILE_NOTIFY_CHANGE_ATTRIBUTES |\
+	FILE_NOTIFY_CHANGE_SIZE |\
+	FILE_NOTIFY_CHANGE_CREATION |\
+	FILE_NOTIFY_CHANGE_SECURITY
+
+#define REG_CHANGE_FLAGS REG_NOTIFY_CHANGE_NAME |\
+	REG_NOTIFY_CHANGE_LAST_SET
+
+
+// RegFsNotify.cpp
+extern HANDLE  g_hStopEvent;	// 이벤트 다루는 핸들
+extern HANDLE  g_hFile;			// 파일 다루는 핸들
+extern HANDLE  g_hRegWatch[2];
+
+void Output_Console(USHORT Color, LPTSTR format, ... );
+void Output_File(LPTSTR format, ...);
+void StartFileMonitor(void);
+void StartRegistryMonitor(void);
+
+// reg.cpp
+extern TCHAR * resultBuffer;
+
+// extractProcessName.cpp
+static int roamingNum = 0;
+static int prefetchNum = 0;
+static int runNum = 0;
+static TCHAR* roamingList[100];
+static TCHAR* prefetchList[100];
+static TCHAR* runList[100];
+
+void ListPrint(void);
+BOOL compare(void);
+void ExtractProcess(DWORD, TCHAR *);
+
+
+// setUser.cpp
+BOOL SetPrivilege(LPCTSTR lpszPrivilege, BOOL bEnablePrivilege);	// setPrivilege
+
+
+// processControl.cpp
+DWORD FindProcessID(LPCTSTR szProcessName);			// 원하는 프로세스의 PID 가져옴
+void printProcessInformation(PROCESSENTRY32 pe);	// 프로세스 정보 출력
+void ListProcessInfo(void);							// 프로세스 목록 출력
+BOOL KillProcess(TCHAR* TargetProcess);				// 프로세스 죽이기
+
+
+// createDummy.cpp
+int makeDummy();
+
+
+// ******************* 화이트리스트 *************************
+
+// 화이트 리스트 목록
+static LPTSTR Roaming_szAllow[] = { 
+	_T("Roaming\\"),
+	_T("AppData\Roaming\\"),
+
+};
+static LPTSTR Prefetch_szAllow[] = {  
+	_T("Windows\Prefetch\\"),
+	_T("Prefetch\\"),
+};
+static LPTSTR Dummy_szAllow[] = {  
+	_T(""),
+	_T("Users\\kosta\\Documents"),
+	// 변수 -> 폴더 1		%EHlF4RD	(C:\\%EHlF4RD)
+	// 변수 -> 폴더 2
+	// 변수 -> 폴더 3
+
+};
+
+// 화이트리스트에 속하는지 여부 확인 함수
+// return 1 : Roaming
+// return 2 : Prefetch
+// return 4 : Dummy File
+// return -1 : Nothing
+static int Whitelisted(LPTSTR szFile)              
+{
+	for(int i=0; i<sizeof(Roaming_szAllow)/sizeof(LPTSTR); i++)
+	{
+		if (_tcsstr(szFile, Roaming_szAllow[i]) != NULL)
+			return 1;
+	}
+
+	for(int i=0; i<sizeof(Prefetch_szAllow)/sizeof(LPTSTR); i++)
+	{
+		if (_tcsstr(szFile, Prefetch_szAllow[i]) != NULL) 
+			return 2;
+	}
+
+	for(int i=0; i<sizeof(Dummy_szAllow)/sizeof(LPTSTR); i++)
+	{
+		if (_tcsstr(szFile, Dummy_szAllow[i]) != NULL) 
+		{
+			return 4;
+		}
+	}
+
+	return -1;
+}
